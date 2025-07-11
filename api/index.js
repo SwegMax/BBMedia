f5c9f5c4522e4948aeef0c52fde96e13c0f002ec
SwegMax <pwftw.jervis@gmail.com>
2025-07-11 17:48:14 +0800

    Feature - Added image upload

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import multer from "multer";
 
 //middlewares
 app.use((req, res, next) => {
@@ -23,15 +24,19 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
+    cb(null, '../client/public/upload')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
 const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
