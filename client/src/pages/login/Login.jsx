9ae469bfc14ebe5e9bc43f767609afd8175231bd
SwegMax <pwftw.jervis@gmail.com>
2025-08-02 14:18:18 +0800

    Fix - Login & Register Screen min width

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -33,13 +33,14 @@ const Login = () => {
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Hello World.</h1>
+          <h1>BBMedia</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
+            Welcome to BBMedia, a React/Node/Express social media web app project by {" "}
+            <a href="https://github.com/SwegMax" target="_blank" rel="noopener noreferrer">
+              SwegMax
+            </a>.
           </p>
-          <span>Don't you have an account?</span>
+          <span>Don't have an account?</span>
           <Link to="/register">
             <button>Register</button>
           </Link>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -30,13 +30,11 @@ const Register = () => {
     <div className="register">
       <div className="card">
         <div className="left">
-          <h1>Lama Social.</h1>
+          <h1>BBMedia</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
+            Don't have an account? Register here!
           </p>
-          <span>Do you have an account?</span>
+          <span>Already have an account?</span>
           <Link to="/login">
           <button>Login</button>
           </Link>
